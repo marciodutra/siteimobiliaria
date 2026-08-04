d67e1d0e93ae67df8284efab112ec432888de1ba
Marcio Dutra <professormarciodutra@gmail.com>
2026-08-04 14:51:02 -0300

    Corrigindo bug das fotos dos imóveis

--- backend/src/routes/imagem.routes.js
+++ backend/src/routes/imagem.routes.js
@@ -1,61 +1,51 @@
 const express = require("express");
 const multer = require("multer");
+const fs = require("fs");
 
 const router = express.Router();
 
 const imagemController = require("../controllers/imagem.controller");
 
+const pastaUpload = "uploads/imoveis";
 
+if (!fs.existsSync(pastaUpload)) {
+    fs.mkdirSync(pastaUpload, {
+        recursive: true
+    });
+}
 
 const storage = multer.diskStorage({
 
-    destination:(req,file,cb)=>{
+    destination(req, file, cb) {
 
-        cb(null,"uploads/imoveis");
+        cb(null, pastaUpload);
 
     },
 
+    filename(req, file, cb) {
 
-    filename:(req,file,cb)=>{
-
-        const nome =
-        Date.now()+"-"+file.originalname;
-
-        cb(null,nome);
+        cb(
+            null,
+            Date.now() + "-" + file.originalname
+        );
 
     }
 
 });
 
-
-
 const upload = multer({
     storage
 });
 
-
-
-
 router.post(
-
     "/:imovelId",
-
-    upload.array("imagens",10),
-
+    upload.array("imagens", 10),
     imagemController.adicionarImagem
-
 );
-
-
 
 router.delete(
-
     "/:id",
-
     imagemController.removerImagem
-
 );
-
-
 
 module.exports = router;
